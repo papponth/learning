f149d1fb141b75ada7e419eac45198dd7ca6f843
papponth <papponth@gmail.com>
2018-03-22 17:14:11 +0300

    3rd

---
+++ Lab1.4/ipaddr.py
@@ -0,0 +1,20 @@
+from ipaddress import *
+from random import *
+
+
+
+class IPv4RandomNetwork(IPv4Network):
+    def __init__(self):
+        IPv4Network.__init__(self,(randrange(0x0B000000, 0xDF000000, 1),(randrange(8, 24, 1))),strict=False)
+
+    def regular(self):
+        return not (self.is_private or self.is_multicast)
+
+
+
+i=0
+while i < 50:
+    ipnet = IPv4RandomNetwork()
+    if ipnet.regular():
+        print(ipnet)
+        i +=1
