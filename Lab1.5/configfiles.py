d517b467f7c37389222fd55a5ca6f644ff52c2c1
papponth <papponth@gmail.com>
2018-03-23 12:17:47 +0300

    5th

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -1,12 +1,11 @@
 import glob
 
-addresslist = []
+addresslist = list()
 
 for file in glob.iglob ("/home/ag/cloud/Seafile/p4ne/p4ne_training/config_files/*.txt"):
     with open(file) as openedfile:
         for lineinfile in openedfile:
-            if "ip address " in lineinfile and not "dhcp" in lineinfile:
-
+            if "ip address" in lineinfile and not any(s.isalpha() for s in " ".join((lineinfile.strip().split(" ")[2:4]))):
                 addresslist.append(" ".join(lineinfile.strip().split(" ")[2:4]))
 
 
