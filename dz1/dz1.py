4bae6711fc00fa03c731d75afabceb7912f9ae86
papponth <papponth@gmail.com>
2018-03-29 15:19:37 +0300

    dz1

--- dz1/dz1.py
+++ dz1/dz1.py
@@ -5,8 +5,8 @@ import openpyxl
 
 def parceconfig (somelinefromfile):
     frsttry = re.match("^\s*ip address ((?:[0-9]{1,3}\.?){4}) ((?:[0-9]{1,3}\.?){4})", str(somelinefromfile))
-    scndtry = re.match("^\s*interface (.*)$", str(somelinefromfile))
-    thrdtry = re.match("^\s*hostname (.*)$", str(somelinefromfile))
+    #scndtry = re.match("^\s*interface (.*)$", str(somelinefromfile))
+    #thrdtry = re.match("^\s*hostname (.*)$", str(somelinefromfile))
     if bool(frsttry):
         ipwithmask = str(frsttry.group(1)) + "/" + str(frsttry.group(2))
         #answer = {"ip": ipaddress.IPv4Interface(ipwithmask)}
@@ -47,9 +47,6 @@ for i in range(len(sortedlist)):
     for j in range(len(str(sortedlist[i]).split("/"))):
         sheet.cell(row=i+1,column=j+1).value = str(sortedlist[i]).split("/")[j]
 book.save('output.xls')
-
-
-
 
 
 
